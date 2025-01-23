aeb1713e5f6d3f63c6094891c065f31f5c80cd2d
nosfertm <nosfertm@gmail.com>
2025-01-23 20:25:01 -0300

    a

--- docs/js/theme.js
+++ docs/js/theme.js
@@ -4,12 +4,11 @@
 export function initTheme() {
     const themeToggle = document.getElementById('themeToggle');
     const currentTheme = localStorage.getItem('theme') || 'light';
-    document.body.classList.add(currentTheme);
+    document.documentElement.setAttribute('data-theme', currentTheme);
 
     themeToggle.addEventListener('click', () => {
-        const newTheme = document.body.classList.contains('dark') ? 'light' : 'dark';
-        document.body.classList.remove('light', 'dark');
-        document.body.classList.add(newTheme);
+        const newTheme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+        document.documentElement.setAttribute('data-theme', newTheme);
         localStorage.setItem('theme', newTheme);
     });
 }
